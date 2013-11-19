31c9753237bff8fab594a81f80f68769283ffe4f
Per Liedman <per@liedman.net>
2013-11-19 14:51:35 +0100

    Swapped to OSM in EPSG:3006 tileset

--- examples/local-projection.js
+++ examples/local-projection.js
@@ -1,21 +1,21 @@
 var osmTileJSON = {
     "tilejson": "2.0.0",
-    "name": "LMV-RT90",
-    "description": "Kartena's rendering of Swedish Lantmäteriverket's map data in RT90 projection.",
+    "name": "osm-bright-3006",
+    "description": "Kartena's rendering, based on MapBox's OSM Bright, of Swedish OpenStreetMap data in SWEREF99 projection.",
     "version": "1.0.0",
-    "attribution": "Map data &copy; Lantmäteriverket, rendering &copy; Kartena AB",
+    "attribution": "Map data &copy; <a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a> contributors; Imagery &copy; 2013 Kartena",
     "scheme": "xyz",
     "tiles": [
-        "http://api.geosition.com/tile/lmv/${z}/${x}/${y}.png",
+    "http://api.geosition.com/tile/osm-bright-3006/{z}/{x}/{y}.png"
     ],
     "minzoom": 0,
     "maxzoom": 14,
-    "crs": "EPSG:2400",
-    "projection": "+lon_0=15.808277777799999 +lat_0=0.0 +k=1.0 +x_0=1500000.0 +y_0=0.0 +proj=tmerc +ellps=bessel +units=m +towgs84=414.1,41.3,603.1,-0.855,2.141,-7.023,0 +no_defs",
+    "crs": "EPSG:3006",
+    "projection": "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ",
     "transform": [1, 0, -1, 0],
-    "scales": [0.0001220703125, 0.000244140625, 0.00048828125, 0.0009765625, 
-               0.001953125, 0.00390625, 0.0078125, 0.015625, 0.03125, 0.0625, 
-               0.125, 0.25, 0.5, 1.0, 2.0],
+    "scales": [0.0001220703125, 0.000244140625, 0.00048828125, 0.0009765625,
+    0.001953125, 0.00390625, 0.0078125, 0.015625, 0.03125, 0.0625,
+    0.125, 0.25, 0.5, 1.0, 2.0],
     "center": [ 11.9, 57.7, 8 ]
 };
 
